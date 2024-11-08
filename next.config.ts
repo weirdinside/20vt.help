f5088a63307af653b1117e1dd1d08173064b1c11
weirdinside <ani@weirdinsi.de>
2024-11-07 19:55:38 -0500

    submitModal changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['ntzseamwgwdavmrdziiy.supabase.co']
+  },
 };
 
 export default nextConfig;
--- src/app/_components/Wheel Gallery/PreviewModal/PreviewModal.tsx
+++ src/app/_components/Wheel Gallery/PreviewModal/PreviewModal.tsx
@@ -1,49 +1,56 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import React, { useEffect, useState, useTransition } from "react";
 import styles from "./PreviewModal.module.css";
-import GalleryItem from "../GalleryItem/GalleryItem";
 import { copyLink } from "@/app/utils";
+import SLogo from "../../Homepage/SLogo/SLogo";
 
 export default function PreviewModal({ activeModal, closeModal, data }) {
   const [imageData, setImageData] = useState({});
-  const [isCopying, startCopying] = useTransition();
+  const [isCopying, setIsCopying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleClickAwayClose(e) {
     e.stopPropagation();
 
     if (e.target.classList.contains(styles["modal"])) {
-      closeModal();
+      handleCloseModal();
     }
   }
 
-  function handleCopyLink(){
-    startCopying(()=>{
-      copyLink(data.photo_url).then((res)=>{
-        console.log(res)
-      })
-    })
-    
+  function handleCloseModal() {
+    closeModal();
+    setIsLoading(true);
+  }
+
+  async function handleCopyLink() {
+    copyLink(data.photo_url).then((res) => {
+      console.log(res);
+      setIsCopying(true);
+      return;
+    });
+    setTimeout(() => {
+      setIsCopying(false);
+    }, 2000);
   }
 
   useEffect(() => {
     function handleEscClose(e) {
       if (e.key === "Escape") {
-        closeModal();
+        handleCloseModal();
       }
     }
     window.addEventListener("keydown", handleEscClose);
     return () => {
       window.removeEventListener("keydown", handleEscClose);
     };
-  });
+  }, []);
 
   useEffect(() => {
     setImageData(data);
   }, [data]);
-
-
 
   return (
     <div
@@ -57,23 +64,59 @@ export default function PreviewModal({ activeModal, closeModal, data }) {
           activeModal === "preview" ? styles["active"] : ""
         }`}
       >
-        <GalleryItem imageInfo={imageData}></GalleryItem>
+        <div className={styles["image-container"]}>
+          <div
+            className={`${styles["image__loading"]} ${
+              isLoading ? styles["active"] : ""
+            }`}
+          ></div>
+          <Image
+            className={styles["image"]}
+            loading="eager"
+            onLoadingComplete={() => {
+              setIsLoading(false);
+            }}
+            decoding="sync"
+            alt="modal-image"
+            src={data.photo_url}
+            quality={100}
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        </div>
+
         <div className={styles["preview-modal__options"]}>
-          {data.submitted_by?.includes('@') ? (
-            <Link target="_blank" href={`https://instagram.com/${data.submitted_by.slice(data.submitted_by.indexOf("@") + 1)}`}>
-            <button className={`${styles["preview-modal__button"]} ${styles['instagram']}`}>
-              more from {data.submitted_by}
-              <div className={styles['preview-modal__button-ib']}></div>
-            </button>
+          {data?.submitted_by?.includes("@") ? (
+            <Link
+              target="_blank"
+              href={`https://instagram.com/${data.submitted_by.slice(
+                data.submitted_by.indexOf("@") + 1,
+              )}`}
+            >
+              <button className={styles["preview-modal__button"]}>
+                instagram link
+              </button>
             </Link>
           ) : null}
 
-          <button disabled={isCopying} onClick={handleCopyLink} className={styles["preview-modal__button"]}>
-            copy link
+          <button
+            onClick={() => {
+              handleCopyLink();
+            }}
+            className={styles["preview-modal__button"]}
+          >
+            copy image link
           </button>
         </div>
       </div>
-      {/* <div className={`${styles['preview-modal__copied']} ${isCopying ? styles['active'] : ''}`}>URL copied</div> */}
+      <div
+        className={`${styles["preview-modal__copied"]} ${
+          isCopying ? styles['active'] : ""
+        }`}
+      >
+        <p className={styles["preview-modal__copied-text"]}>link copied!</p>
+      </div>
     </div>
   );
 }
